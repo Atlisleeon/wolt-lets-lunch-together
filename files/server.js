c6111ddeea33b485fda3699fb34c50d56d0d8c1d
Lion Atlis <lionatlis@CQRC7G4WJHMacBook-Air.local>
2026-05-04 06:32:39 +0300

    fix CORS

--- files/server.js
+++ files/server.js
@@ -6,7 +6,9 @@ const { JWT } = require('google-auth-library');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // ─── Config ───────────────────────────────────────────────────────────────────
